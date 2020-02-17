8c4000c47cce5b6989cb6347cf1308a18a0816d5
RuqayyahSara <sarasadiya4@gmail.com>
2020-02-17 20:29:29 +0530

    Creating reducers and importing it to main index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
 import {applyMiddleware,createStore} from "redux";
+import reducers from "./reducers";
 const App=()=>{
 return(
     <div>
         <h1>Hello World</h1>
     </div>
-)
-}
+);
+};
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware()}>
+    <Provider store={createStoreWithMiddleware(reducers)}>
         <App/>
     </Provider>, document.getElementById('root'));
 
